4632f79f86f3e1dab52b2ef40c09848da6c53ffd
Andres Abad Rodriguez <andres.abad.rodriguez@cern.ch>
2013-11-29 13:59:38 +0000

    Details table working (not loading data yet)

--- js/jobs.js
+++ js/jobs.js
@@ -1,14 +1,13 @@
 function loadJobTable(jobList){
-//	job_id 
-//	submit_time
-//	source_se
-//	dest_se
-//	
-//	job_state
 	$("#jobResultsTable > tbody").html("");
 	$.each(jobList, function(index, value){
-		var t_row = '<tr class="' + getRowColor(value.job_state) + '" value="' + value.job_id + '">' + getColumn(value.job_id) 
-					+ getColumn(value.submit_time) + getColumn(value.source_se) + getColumn(value.dest_se) + '</tr>'  ;		
+		//Transfer row
+		var t_row = '<tr class="' + getRowColor(value.job_state) + ' accordion-body" data-toggle="collapse" id="' + value.job_id 
+		+ '" data-target="#' + value.job_id + '_row" onclick="toogleDetailRowState(\'' + value.job_id + '_row\')">' + getColumn(value.job_id) + getColumn(value.submit_time) 
+		+ getColumn(value.source_se) + getColumn(value.dest_se) + '</tr>'  ;		
+		$("#jobResultsTable > tbody:last").append(t_row);
+		//Jobs from transfer row (hidden by default)
+		t_row = '<tr class="collapse out" style="display: none;" id="' + value.job_id + '_row"><td colspan="4" ><table><tr><td> Demo1</td></tr> </table> </td></tr>';
 		$("#jobResultsTable > tbody:last").append(t_row);
 	});
 }
@@ -26,3 +25,20 @@ function getRowColor(job_state){
 		return "warning";
 	}
 }
+
+function showUserError(message){
+	$('#serverErrorText').text(message);
+	$('#serverkeyAlert').show();
+}
+
+function hideUserError(){	
+	$('#serverkeyAlert').hide();
+}
+
+function toogleDetailRowState(rowId) {	
+	if ($("#jobResultsTable > tbody > #" + rowId).css('display') == 'none') {
+		$("#jobResultsTable > tbody > #" + rowId).show();
+	} else {
+		$("#jobResultsTable > tbody > #" + rowId).hide();
+	}
+}	
