8f276aa539c01545b5f59e71b60e3c07b6ec2f22
aabadz <andres.abad.rodriguez@cern.ch>
2014-03-25 17:18:36 +0100

    New index content asked by Oliver

--- index.php
+++ index.php
@@ -30,49 +30,33 @@
 		<div class="masthead">
 			<h3 class="text-muted">webFTS (Beta version)</h3>			
 			<ul class="nav nav-justified">
-				<li class="active"><a href="#">Home</a></li>
-				<li><a href="transmissions.php">My jobs</a></li>
-				<li><a href="submit.php">Submit a transfer</a></li>
+				<li class="active"><a href="#"><i class="glyphicon glyphicon-home"></i>&nbsp;Home</a></li>
+				<li><a href="transmissions.php"><i class="glyphicon glyphicon-tasks"></i>&nbsp;My jobs</a></li>
+				<li><a href="submit.php"><i class="glyphicon glyphicon-chevron-left"></i><i class="glyphicon glyphicon-chevron-right"></i>&nbsp;Submit a transfer</a></li>				
 			</ul>
 		</div>
 		<div>
 			&nbsp;
 		</div>
 		<div>
-			<h2>  <span class="label label-info">Do you need to move files between Grid machines?
+			<h2> <span class="label label-info"><i class="glyphicon glyphicon-arrow-right"></i>&nbsp;Do you need to move files between grid machines?
 			</span></h2> 
-			<h2>  <span class="label label-info"> Do you think command line interfaces are too complicated? 
+			<h2> <span class="label label-info"><i class="glyphicon glyphicon-arrow-right"></i>&nbsp;Looking for a graphical interface to manage your transfers? 
 			</span></h2>
-			<h1> <span class="label label-success">Start to use <strong>webFTS</strong> today and discover how easy is to transfer files in the Grid! 
+			<h1> <span class="label label-success">Start to use <strong>webFTS</strong> today and discover how easy it is to transfer files on the grid! 													
 			</span></h1>
-			<h3> <span class="label label-default pull-right">WebFTS is powered by FTS3. 
-			</span></h3>
 		</div>
-
-		<!-- Example row of columns -->
-		<div class="row">
-			<div class="col-lg-4">
-				<h2>Information</h2>
-				<p>More information about WebFTS and the user guide can be found in the official page.</p>
-				<p>
-					<a class="btn btn-primary" href="https://webfts.web.cern.ch/content/user-guide">View details &raquo;</a>
-				</p>
-			</div>
-			<div class="col-lg-4">
-				<h2>EUDAT</h2>				
-				<p>
-					<a class="btn btn-primary" href="http://www.eudat.eu/">View details &raquo;</a>
-				</p>
-			</div>
-			<div class="col-lg-4">
-			</div>
-		</div>
-
 		<!-- Site footer -->
-		<div class="footer">
-			<p>&copy; CERN 2013</p>
-		</div>
-
+		<div class="footer">			
+				<h3>
+				<span class="label label-primary"><i class="glyphicon glyphicon-info-sign"></i>&nbsp;More information about WebFTS and the user guide can be found in the official page.</span>
+				
+				<a class="btn btn-sm btn-primary" href="https://webfts.web.cern.ch/content/user-guide">View details &raquo;</a>
+				</h3>
+			
+			<div class="row">
+				<p class="pull-right">WebFTS is powered by FTS3. &copy; CERN - EUDAT, 2014</p>
+			</div>
 	</div>
 </body>
 </html>
