8c76957239a1fbbbd96a5daf173ba7989870a6d9
christina-skarpi <christina.skarpi@gmail.com>
2014-08-21 10:50:52 +0200

    New file for the transmissions page.

--- js/site-tourTransmissions.js
+++ js/site-tourTransmissions.js
@@ -1,10 +1,11 @@
 var myIntro = introJs(),
     steps = [{
         element: '#idjobsTable',
-        intro: "<h4>In this area are going to be displayed all your submitted jobs! Check the live status of your submittions whenever you want!</br><strong>Are you ready to start?</strong></h4>"
+        intro: "<h3><strong>Congratulations!</strong></h3></br><h4>In this area are going to be displayed all your submitted jobs! Check the live status of your submittions whenever you want!</br><strong>Are you ready to start?</strong></h4>"
         ,position: "bottom-middle-aligned"
     }];
 
+myIntro.setOption("showStepNumbers", "false");
 myIntro.setOptions({
     steps: steps
 });
