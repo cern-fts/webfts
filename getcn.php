d348452f506eb4228b4e4a48df57cca990af5a14
andrea-manzi <andrea.manzi@gmail.com>
2014-10-27 10:12:02 +0100

    restrict access only to ajax call to getcn.php

--- getcn.php
+++ getcn.php
@@ -1,9 +1,13 @@
 <?php
-                foreach($_SERVER as $h=>$v){
-                        if ($h == "SSL_CLIENT_S_DN_CN_2")
-                                $clientCN = $v;
-                        else if ($h == "SSL_CLIENT_S_DN_CN")
-                                $clientCN = $v;
-                }
-                echo shell_exec("./utils/getCNfromCertificate.sh \"$clientCN\"");
+// No direct access to this file 
+define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'); 
+if(!IS_AJAX) {die('Restricted access');}
+
+foreach($_SERVER as $h=>$v){
+      if ($h == "SSL_CLIENT_S_DN_CN_2")
+         $clientCN = $v;
+      else if ($h == "SSL_CLIENT_S_DN_CN")
+          $clientCN = $v;
+      }
+echo shell_exec("./utils/getCNfromCertificate.sh \"$clientCN\"");
 ?>
