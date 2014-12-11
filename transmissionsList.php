0743262d4a763b0cf03ccf69bd7bf824b7c4cfc3
andrea-manzi <andrea.manzi@gmail.com>
2014-12-11 15:44:00 +0100

    moved delegation funtion to common.js

--- transmissionsList.php
+++ transmissionsList.php
@@ -3,74 +3,10 @@ $( document ).ready(function() {
 
   //trying to check if a cert from STS has been already stored in the session,
   //otherwise it tries to get one, if it fails goes back to old delegation method
-  if (!sessionStorage.userCert) {
-        $.get("ssoGetAssertion.php", function(data) {
-        
-	//chcking assertion lifetime
-        if (ssoAssertionTimeLeft(data) < 60) {
-                console.log("refreshing sso");
-                //we have to refresh the assertion
-                var currenturl = document.URL;
-                currentpage = currenturl.substring(currenturl.lastIndexOf('/')+1);
-                window.open(sessionStorage.ssoLogin+currentpage,"_self");
-        }
+  getDelegation();
+  //Reload page every 5 minutes (5 * 60 * 1000)
+  var intervalID = window.setInterval(reloadJobs, 300000);
 
-	// Let's check if we really got an assertion
-        var err = ssoErrorString(data);
-        if(err) {
-                console.log(err);
-                getDelegationID("delegation_id", false);
-                return;
-        }
-
-	// We will now generate an RSA keypair *** THIS DOES NOT WORK WITH STS YET ***
-//	var kp = KEYUTIL.generateKeypair("RSA", 2048);
-//	sessionStorage.userKey = hextob64(KEYUTIL.getHexFromPEM(KEYUTIL.getPEM(kp["prvKeyObj"], "PKCS8PRV")));
-
-	// We will now wrap fetched assertion in SOAP envelope
-	// Third parameter to this function is an optional public key from our side (BASE64-encoded)
-//	var req = ssoSoapReq(data, sessionStorage.stsAddress, hextob64(KEYUTIL.getHexFromPEM(KEYUTIL.getPEM(kp["pubKeyObj"]))), []);
-	var req = ssoSoapReq(data, sessionStorage.stsAddress);
-
-        // We will now send our SOAP request to STS
-        if(req) {
-                $.ajax({url: "/sts",
-                type: "POST",
-                contentType: "text/xml; charset=utf-8",
-                headers: {SOAPAction: sessionStorage.stsAddress},
-                data: req}).done(
-                function(data) {
-
-                        // Let's check if STS was happy with our assertion
-
-                        var err = ssoErrorString(data);
-                        if(err) {
-                                console.log(err);
-                                getDelegationID("delegation_id", false);
-                                return;
-                        }
-
-                        // This function returns BASE64-encoded string of generated certificate
-                        var cert = ssoGetCertificate(data);
-                        sessionStorage.userCert = cert;
-
-                        // This function returns BASE64-encoded string of generated private key
-                        var pkey = ssoGetPrivateKey(data);
-                        if(pkey) { // STS provided us a key
-                                sessionStorage.userKey = pkey;
-                        } else { // Use our own key
-                                pkey = sessionStorage.userKey;
-                        }
-
-                        getDelegationIDSTS("delegation_id", false, cert, pkey);
-
-                });
-                }
-        });
-   } else getDelegationIDSTS("delegation_id", false, sessionStorage.userCert, sessionStorage.userKey);
-
-	//Reload page every 5 minutes (5 * 60 * 1000)
-	var intervalID = window.setInterval(reloadJobs, 300000);
 });
 
 $(function(){
