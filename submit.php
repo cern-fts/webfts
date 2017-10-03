946533bdb6d3264befdc09cc032b50757e1d1963
Owayss Kabtoul <owayss.kabtoul@cern.ch>
2017-10-03 18:02:24 +0200

    submit.php: fixes file indentation & removes extra whitespace

--- submit.php
+++ submit.php
@@ -8,7 +8,7 @@
 <meta name="keywords" content="" />
 
 <script src="js/jquery.min.js"></script>
-<script src="js/jquery.validate.min.js"></script> 
+<script src="js/jquery.validate.min.js"></script>
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/bootstrap-theme.min.css" rel="stylesheet">
@@ -25,8 +25,8 @@
 <script src="js/jquery-ui.min.js"></script>
 <script src="js/jquery.finderSelect.min.js"></script>
 <script src="js/jquery.ddslick.min.js"></script>
-<script src="js/bootstrap.min.js"></script> 
-<script src="js/common.js"></script>  
+<script src="js/bootstrap.min.js"></script>
+<script src="js/common.js"></script>
 <script src="js/lib/sha512.js"></script>
 <script src="js/jsrsasign-latest-all-min.js"></script>
 
@@ -45,53 +45,53 @@
 
 <script>
 $( document ).ready(function() {
-        if(!sessionStorage.ftsRestEndpoint)
-                getConfig();
+    if(!sessionStorage.ftsRestEndpoint)
+        getConfig();
 });
 </script>
 
 
-<script> 
-	$(function(){
-	   $("#includedContentTransfer").load("transfer.php"); 
-	});
+<script>
+$(function(){
+    $("#includedContentTransfer").load("transfer.php");
+});
 </script>
 </head>
 <body>
-			<div class='container-top-outer'>
-				<div class="container-top-inner" >
-					<div class="row">
-						<script> 
-							$(function(){
-							   $("#userAuth").load("userAuth.php"); 
-							});
-						</script>
-						<div id="userAuth"></div>
-						<script> 
-							$(function(){
-							   $("#delegateBtn").load("delegateButton.html"); 
-							});
-						</script>
-						<div id="delegateBtn"></div>
-					</div>
-					
-					<div class="masthead">
-						<h3 class="text-muted">WebFTS <small><em>Simplifying power</em></small></h3>
-						<ul class="nav nav-justified">
-							<li><a href="index.php"><i class="glyphicon glyphicon-home"></i>&nbsp;Home</a></li>
-							<li><a href="transmissions.php"><i class="glyphicon glyphicon-tasks"></i>&nbsp;My jobs</a></li>
-							<li class="active" id="start2ndTour"><a href="#" href='javascript:void(0);'><i class="glyphicon glyphicon-chevron-left"></i><i class="glyphicon glyphicon-chevron-right"></i>&nbsp;Submit a transfer</a></li>
-						</ul>
-					</div>
-				<img src="paraFiles/img/shadow-bottom.png" alt=' ' class='divider' >
-				</div>
-				
-			</div>
-			<div class="main-back">
-				<div class="row">
-					<div id="includedContentTransfer"></div>
-				</div>
-			</div>
-	<script type="text/javascript" src="/js/site-tourTransmissions.js"></script>
+            <div class='container-top-outer'>
+                <div class="container-top-inner" >
+                    <div class="row">
+<script>
+$(function(){
+    $("#userAuth").load("userAuth.php");
+});
+</script>
+                        <div id="userAuth"></div>
+<script>
+$(function(){
+    $("#delegateBtn").load("delegateButton.html");
+});
+</script>
+                        <div id="delegateBtn"></div>
+                    </div>
+
+                    <div class="masthead">
+                        <h3 class="text-muted">WebFTS <small><em>Simplifying power</em></small></h3>
+                        <ul class="nav nav-justified">
+                            <li><a href="index.php"><i class="glyphicon glyphicon-home"></i>&nbsp;Home</a></li>
+                            <li><a href="transmissions.php"><i class="glyphicon glyphicon-tasks"></i>&nbsp;My jobs</a></li>
+                            <li class="active" id="start2ndTour"><a href="#" href='javascript:void(0);'><i class="glyphicon glyphicon-chevron-left"></i><i class="glyphicon glyphicon-chevron-right"></i>&nbsp;Submit a transfer</a></li>
+                        </ul>
+                    </div>
+                <img src="paraFiles/img/shadow-bottom.png" alt=' ' class='divider' >
+                </div>
+
+            </div>
+            <div class="main-back">
+                <div class="row">
+                    <div id="includedContentTransfer"></div>
+                </div>
+            </div>
+    <script type="text/javascript" src="/js/site-tourTransmissions.js"></script>
 </body>
 </html>
