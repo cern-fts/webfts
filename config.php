2e20ab4434daded37fffd8f084f4b0989729cd4b
Joshua Bachmeier <uwdkl@student.kit.edu>
2018-06-15 16:13:38 +0200

    Add php config file with example OP

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'oidc_provider' => array(
+        array(
+            'description' => 'Example',
+            'issuer' => 'http://foo',
+            'client_id' => 'bar',
+            'client_secret' => 'baz',
+        ),
+    ),
+);
+?>
