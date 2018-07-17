12ce2fa53cc70ac52cd234a6548db030f6326363
Joshua Bachmeier <uwdkl@student.kit.edu>
2018-07-17 15:17:29 +0200

    Fix error response code

--- src/FTS3Exception.php
+++ src/FTS3Exception.php
@@ -2,5 +2,11 @@
 
 use Exception;
 
-class FTS3Exception extends Exception {}
+class FTS3Exception extends Exception {
+    public getCode() {
+        $code = parent::getCode();
+
+        return $code != 0 ? $code : 500;
+    }
+}
 ?>
