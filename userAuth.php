0386184a45f277cb3c4169d80206144dbe6d3de7
Edward Karavakis <edward.karavakis@cern.ch>
2020-05-04 13:37:49 +0000

    Update userAuth.php

--- userAuth.php
+++ userAuth.php
@@ -15,7 +15,7 @@ if ($('#sslalert').text() !== "") {
 					{
 					    echo "<div id='sslalert'><span><strong>You are not providing a user certificate</strong></span></div>";  
         			 	} 		
-                    elseif (isset($_SERVER['SSL_CLIENT_S_DN_CN_2'])) {
+					elseif (isset($_SERVER['SSL_CLIENT_S_DN_CN_2'])) {
 						echo "<div id='auth'><span>You are authenticated as <strong>{$_SERVER['SSL_CLIENT_S_DN_CN_2']}</strong></span></div>";
 					} elseif ($_SERVER['SSL_CLIENT_S_DN_CN']) {
 						echo "<div id ='auth'><span>You are authenticated as <strong>{$_SERVER['SSL_CLIENT_S_DN_CN']}</strong></span></div>";
